9a9de9305c2cf3d00b64a0aca27f59e117e8e561
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-01-30 02:38:43 +0530

    cors policy changed

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get("/", (req, res) => {
     res.send(`<h2>Welcome to Internative coding School</h2>`);
